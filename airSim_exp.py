36338db4447fba4731cf0370f3b65e5a3557a277
Faisal Ahmed <39379134+FaisalAhmed0@users.noreply.github.com>
2020-09-26 17:34:34 +0000

    Update airSim_exp.py

--- airSim_exp.py
+++ airSim_exp.py
@@ -12,10 +12,10 @@ def envFunc():
 env_fn = envFunc
 ac_kwargs = dict(hidden_sizes=[64, 64])
 logger_kwargs = dict(output_dir='/home/faisal/Documents/airsim_exp_results_seed_0',exp_name='airsim_fixed_goal')
-ppo(env_fn=envFunc, ac_kwargs=ac_kwargs, seed= 0 ,max_ep_len=400, steps_per_epoch=4000, epochs=375, logger_kwargs=logger_kwargs)
+ppo(env_fn=envFunc, ac_kwargs=ac_kwargs, seed= 1234 ,max_ep_len=400, steps_per_epoch=4000, epochs=375, logger_kwargs=logger_kwargs)
 logger_kwargs = dict(output_dir='/home/isra/Documents/airsim_exp_results_seed_5',exp_name='airsim_fixed_goal')
-ppo(env_fn=envFunc, ac_kwargs=ac_kwargs, seed= 5 ,max_ep_len=400, steps_per_epoch=4000, epochs=375, logger_kwargs=logger_kwargs)
+ppo(env_fn=envFunc, ac_kwargs=ac_kwargs, seed= 12345 ,max_ep_len=400, steps_per_epoch=4000, epochs=375, logger_kwargs=logger_kwargs)
 logger_kwargs = dict(output_dir='/home/isra/Documents/airsim_exp_results_seed_10',exp_name='airsim_fixed_goal')
-ppo(env_fn=envFunc, ac_kwargs=ac_kwargs, seed= 10 ,max_ep_len=400, steps_per_epoch=4000, epochs=375, logger_kwargs=logger_kwargs)
+ppo(env_fn=envFunc, ac_kwargs=ac_kwargs, seed= 123456 ,max_ep_len=400, steps_per_epoch=4000, epochs=375, logger_kwargs=logger_kwargs)
 
 
